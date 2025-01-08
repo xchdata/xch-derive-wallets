9aa01cad009fddaaba360c1768fb5134d69a9e5f
earl <contact@xchdata.io>
2025-01-08 20:58:49 +0100

    Update from chia-bls & chia-wallet to umbrella chia package

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use bech32::ToBase32;
-use chia_bls::{derive_keys, PublicKey};
-use chia_wallet::{
-    standard::standard_puzzle_hash, standard::DEFAULT_HIDDEN_PUZZLE_HASH, DeriveSynthetic,
-};
+use chia::bls::PublicKey;
+use chia::puzzles::{standard::StandardArgs, DeriveSynthetic};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -25,9 +23,9 @@ fn main() -> eyre::Result<()> {
     let pk_bytes = hex::decode(pk_hexstr)?;
     let pk = PublicKey::from_bytes(pk_bytes.as_slice().try_into()?)?;
     for i in args.index..args.count {
-        let child_pk = derive_keys::master_to_wallet_unhardened(&pk, i);
-        let child_pk_syn = child_pk.derive_synthetic(&DEFAULT_HIDDEN_PUZZLE_HASH);
-        let ph = standard_puzzle_hash(&child_pk_syn);
+        let child_pk = chia::bls::master_to_wallet_unhardened(&pk, i);
+        let child_pk_syn = child_pk.derive_synthetic();
+        let ph = StandardArgs::curry_tree_hash(child_pk_syn);
         let ph_hexstr = hex::encode(ph);
         let addr = bech32::encode("xch", ph.to_base32(), bech32::Variant::Bech32m)?;
         println!("{}\t{}", ph_hexstr, addr);
